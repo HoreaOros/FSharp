c9c71b49c344d5561d9fe6f3b9c51d6aa97270a6
Horea Oros <horea.oros@gmail.com>
2015-03-13 14:02:37 +0200

    first F# program

---
+++ SDI/HelloWorld.fs
@@ -0,0 +1,22 @@
+﻿module HelloWorld
+open System
+
+let cube x = x * x * x
+
+[<EntryPoint>]
+let main (args : string[]) =
+    if args.Length <> 2 then
+        failwith "Error: Expected arguments <greeting> and <thing>"
+    let greeting, thing = args.[0], args.[1]
+    let timeOfDay = System.DateTime.Now.ToString("hh:mm tt")
+    printfn "%s, %s at %s" greeting thing timeOfDay
+
+
+    let x = 1 // se creeza valoarea x de tip int
+    let y = 2.3 // se creeaza valoarea y de tip float
+    printfn "%f" (float x + y) // pentru a putea aduna cele doua valori de tip diferit, x trebuie convertit in float
+
+    printfn "%d" (cube 4)
+    // Program exit code
+    0
+
--- SDI/Program.fs
+++ SDI/Program.fs
@@ -1,7 +1,9 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿module Hello
+//// Learn more about F# at http://fsharp.net
+//// See the 'F# Tutorial' project for more help.
+//
+//[<EntryPoint>]
+//let main argv = 
+//    //printfn "%A" argv
+//    printfn "Hello World of F#"
+//    0 // return an integer exit code
